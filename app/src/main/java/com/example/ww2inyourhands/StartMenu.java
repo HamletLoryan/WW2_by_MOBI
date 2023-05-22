20cff1826d6657fb0e8a42470424e0e01ce3434c
Hamlet <loryanhamlet@gmail.com>
2023-05-22 21:46:21 +0400

    22.05.2023/21.45

--- app/src/main/java/com/example/ww2inyourhands/StartMenu.java
+++ app/src/main/java/com/example/ww2inyourhands/StartMenu.java
@@ -3,9 +3,16 @@ package com.example.ww2inyourhands;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.Window;
 import android.widget.Button;
+
+import java.io.IOException;
 
 public class StartMenu extends AppCompatActivity {
 
@@ -16,6 +23,12 @@ public class StartMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_menu);
+
+        try {
+            if(!isOnline()) showDialog(StartMenu.this);
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
 
         playBtn = findViewById(R.id.play_button);
         hallOfFameBtn = findViewById(R.id.hall_of_fame_button);
@@ -41,6 +54,32 @@ public class StartMenu extends AppCompatActivity {
     public void onBackPressed(){
 
     }
+    private void showDialog(Activity activity) {
+
+        final Dialog dialog = new Dialog(activity);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setCancelable(false);
+        dialog.setContentView(R.layout.offline_start_dialog_otp);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        Button dialogBtn_continue = dialog.findViewById(R.id.ok_btn);
+        dialogBtn_continue.setOnClickListener(v -> dialog.dismiss());
+        Button dialogBtn_log_in = dialog.findViewById(R.id.close_btn);
+        dialogBtn_log_in.setOnClickListener(v -> {
+            android.os.Process.killProcess(android.os.Process.myPid());
+            System.exit(1);
+        });
+        dialog.show();
+    }
+
+    public boolean isOnline() throws IOException, InterruptedException {
+        Runtime runtime = Runtime.getRuntime();
+        Process ipProcess = runtime.exec("/system/bin/ping -c 1 8.8.8.8");
+        int     exitValue = ipProcess.waitFor();
+        return (exitValue == 0);
+
+    }
+
+    /* TODO: make a close app function */
 
 
 
