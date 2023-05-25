4fcf6599a9daff86684d7333041b319d875a7c3b
Hamlet <loryanhamlet@gmail.com>
2023-05-25 21:56:01 +0400

    25.05.2023/21.55

--- app/src/main/java/com/example/ww2inyourhands/Story.java
+++ app/src/main/java/com/example/ww2inyourhands/Story.java
@@ -17,6 +17,10 @@ public class Story {
             case "Commander": commander(); break;
             case "Start Point": startPoint(); break;
             case "The Mission starts": theMissionStarts(); break;
+            case "Stealth": stealth(); break;
+            case "Vantage point": vantagePoint(); break;
+            case "Locals": locals(); break;
+            case "Camp": camp(); break;
 
         }
 
@@ -82,11 +86,78 @@ private void showAllButtons(){
 
         nextPositionA = "Stealth";
         nextPositionB = "Vantage point";
-        nextPositionC = "Information";
+        nextPositionC = "Locals";
         nextPositionD = "Camp";
 
         currentPosition = "The Mission starts";
     }
+
+    public void stealth(){
+        gs.sceneImage.setImageResource(R.drawable.forest);
+        gs.sceneText.setText(R.string.stealth);
+
+        gs.variantABtn.setText(R.string.stealthBtnA);
+        gs.variantBBtn.setText(R.string.stealthBtnB);
+        gs.variantCBtn.setText(R.string.stealthBtnC);
+        gs.variantDBtn.setText(R.string.stealthBtnD);
+
+        nextPositionA = "Observation";
+        nextPositionB = "Camp infiltration";
+        nextPositionC = "Circling around";
+        nextPositionD = "Retreat from forest";
+
+        currentPosition = "Stealth";
+    }
+    public void vantagePoint(){
+        gs.sceneImage.setImageResource(R.drawable.binoculars);
+        gs.sceneText.setText(R.string.vantagePoint);
+
+        gs.variantABtn.setText(R.string.vantagePointBtnA);
+        gs.variantBBtn.setText(R.string.vantagePointBtnB);
+        gs.variantCBtn.setText(R.string.vantagePointBtnC);
+        gs.variantDBtn.setText(R.string.vantagePointBtnD);
+
+        nextPositionA = "Observe from hiding";
+        nextPositionB = "Interception";
+        nextPositionC = "Binoculars";
+        nextPositionD = "Report";
+
+        currentPosition = "Vantage point";
+    }
+    public void locals(){
+        gs.sceneImage.setImageResource(R.drawable.village);
+        gs.sceneText.setText(R.string.locals);
+
+        gs.variantABtn.setText(R.string.localsBtnA);
+        gs.variantBBtn.setText(R.string.localsBtnB);
+        gs.variantCBtn.setText(R.string.localsBtnC);
+        gs.variantDBtn.setText(R.string.localsBtnD);
+
+        nextPositionA = "Marketplace";
+        nextPositionB = "Observation point";
+        nextPositionC = "Villager";
+        nextPositionD = "Eavesdrop";
+
+        currentPosition = "Locals";
+    }
+    public void camp(){
+        gs.sceneImage.setImageResource(R.drawable.forest_camp);
+        gs.sceneText.setText(R.string.camp);
+
+        gs.variantABtn.setText(R.string.campBtnA);
+        gs.variantBBtn.setText(R.string.campBtnB);
+        gs.variantCBtn.setText(R.string.campBtnC);
+        gs.variantDBtn.setText(R.string.campBtnD);
+
+        nextPositionA = "Enemy targets";
+        nextPositionB = "Supply";
+        nextPositionC = "Setting defend.";
+        nextPositionD = "Preparation";
+
+        currentPosition = "Camp";
+    }
+
+
 
 
 
